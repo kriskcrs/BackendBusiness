21d5c304bcf06dc060847e1887b56fefd8ff42b2
Cristian Cáceres <ccaceresm1@miumg.edu.gt>
2023-06-20 21:51:27 -0600

    creacion del proyecto

---
+++ src/main/java/com/business/backendBusiness/BackendBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.business.backendBusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/business/backendBusiness/BackendBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.backendBusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
